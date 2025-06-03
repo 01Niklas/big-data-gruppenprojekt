3a7ba368fa184762a6d9d29b19cd6a7bd6c3183f
Niklas <115569956+01Niklas@users.noreply.github.com>
2025-06-03 18:00:32 +0200

    Add collaborative filtering recommender implementation.

---
+++ gruppenprojekt/collaborative_filtering_recommender.py
@@ -0,0 +1,144 @@
+from typing import Optional, Literal
+
+import numpy as np
+import pandas as pd
+from loguru import logger
+from sklearn.neighbors import NearestNeighbors
+
+
+class CollaborativeFilteringRecommender:
+    def __init__(self, data: pd.DataFrame, mode: Literal['user', 'item'] = 'user') -> None:
+        self.original_data = data
+        self.mode = mode
+        self.data = None
+        self._preprocess_data()
+        self.user_id = None
+        self.item_id = None
+        self.k = 3  # default
+        self.similarity = "cosine"  # default
+        self.calculation_variant = "weighted"  # default
+
+    def _preprocess_data(self) -> None:
+        self.original_data = self.original_data.set_index("user_ID")
+        self.original_data.index = self.original_data.index.astype(str) # convert the index to string (due to error with int values)
+        if self.mode == 'item':
+            self.data = self.original_data.T  # transpose for item based
+        else:
+            self.data = self.original_data  # original for user based
+
+    def _prepare_information(self, user_id: str, item_id: str, similarity: str, calculation_variant: str, k: int) -> None:
+        self.user_id = user_id
+        self.item_id = item_id
+        self.similarity = similarity
+        self.calculation_variant = calculation_variant
+        self.k = k
+
+    def _calculate_distance_and_indices(self, dataframe: pd.DataFrame) -> ([], []):
+        knn = NearestNeighbors(metric=self.similarity, algorithm='brute')
+        knn.fit(dataframe.values)
+        distances, indices = knn.kneighbors(dataframe.values, n_neighbors=self.k + 1)
+
+        if self.mode == 'item':
+            index = self.data.index.get_loc(self.item_id)
+            logger.debug(f"Index of item: {self.item_id} is {index}")
+        else:
+            index = self.data.index.get_loc(self.user_id)
+            logger.debug(f"Index of user: {self.user_id} is {index}")
+
+        similar_distances = distances[index, 1:]
+        similar_indices = indices[index, 1:]
+        logger.debug(f"Similar distances: {similar_distances}")
+        logger.debug(f"Similar indices: {similar_indices}")
+
+        return similar_distances, similar_indices
+
+    def _calculate_similarities(self, similar_distances: np.ndarray) -> np.ndarray:
+        if self.similarity == 'cosine':
+            similarity = [1 - x for x in similar_distances]
+            similarity = [(y + 1) / 2 for y in similarity]
+            return np.array(similarity)
+        else:
+            # TODO: add pearson?
+            raise ValueError("Unsupported similarity metric.")
+
+    def _calculate_result(self, similarity: np.ndarray, ratings: np.ndarray) -> float:
+        if self.calculation_variant == "weighted":
+            mean = np.dot(ratings, similarity) / similarity.sum()
+            logger.debug(f"{self.calculation_variant} Mittelwert: {mean}")
+            return mean
+        else:
+            return float(np.mean(ratings))
+
+
+    def _check_values(self) -> None:
+        if self.mode == 'user':
+            if self.user_id not in self.data.index:
+                raise ValueError(f"User {self.user_id} nicht in Daten.")
+            if self.item_id not in self.data.columns:
+                raise ValueError(f"Item {self.item_id} nicht in Daten.")
+        elif self.mode == 'item':
+            if self.user_id not in self.original_data.index:
+                raise ValueError(
+                    f"User {self.user_id} nicht in Originaldaten.")
+            if self.item_id not in self.data.index:
+                raise ValueError(
+                    f"Item {self.item_id} nicht in transponierten Daten.")
+
+
+    def _process_item_based(self) -> pd.DataFrame:
+        user_ratings = self.original_data.loc[self.user_id]
+
+        # filter based on the item. Only the users that already gave a rating are relevant
+        rated_items = user_ratings[user_ratings > 0.0].index.tolist()
+
+        if not rated_items:
+            raise ValueError(f"User {self.user_id} hat keine Items bewertet!")
+
+        return self.data.loc[rated_items + [self.item_id]]
+
+    def _process_user_based(self):
+        # filter based on the item. Only the users that already gave a rating are relevant
+        relevant_df = self.data[self.data[self.item_id] > 0.0]
+
+        # add the user we are looking for (due to non-existing rating this user where filtered out)
+        return pd.concat([relevant_df, self.data.loc[[self.user_id]]])
+
+    def predict(self, user_id: str, item_id: str, similarity: Literal['cosine', 'pearson'] = 'cosine', calculation_variety: Literal['weighted', 'unweighted'] = 'weighted', k: Optional[int] = 3):
+        self._prepare_information(user_id, item_id, similarity, calculation_variety, k)
+        logger.debug(f"User ID: {self.user_id}, Item ID: {self.item_id}")
+        self._check_values()
+
+        if self.mode == 'item':
+            relevant_df = self._process_item_based()
+        else:
+            relevant_df = self._process_user_based()
+
+        # make sure that there are no NaN values -> set NaN to 0.0
+        relevant_df = relevant_df.fillna(0.0)
+        similar_distances, similar_indices = self._calculate_distance_and_indices(dataframe=relevant_df)
+
+        if self.mode == 'item':
+            ratings = relevant_df.iloc[similar_indices][self.user_id].to_numpy()
+        else:
+            ratings = relevant_df.iloc[similar_indices][self.item_id].to_numpy()
+
+        similarity = self._calculate_similarities(similar_distances)
+        result = self._calculate_result(similarity, ratings)
+
+        self.explain(similar_indices, relevant_df, ratings, similarity, result)
+        return result
+
+    def explain(self, similar_indices, relevant_df, ratings, similarity, result) -> None:
+        print("-" * 50)
+        print(f"<mode: {self.mode}>")
+        print(f"({self.calculation_variant}) Mittelwert: {result:.4f}")
+        print(f"Metrik: {self.similarity}")
+        print()
+        print(f"k ({self.k}) ähnlichsten {'Items' if self.mode == 'item' else 'Nutzer'}:")
+        df = pd.DataFrame({
+            "ID": relevant_df.index[similar_indices],
+            "rating": ratings,
+            "similarity": similarity
+        }).reset_index(drop=True)
+        print(df.to_string(index=True, header=True))
+        print("-" * 50)
--- gruppenprojekt/main.py
+++ gruppenprojekt/main.py
@@ -1,10 +1,16 @@
 # this is just for testing
 import pandas as pd
 
-from gruppenprojekt.user_based_recommender import UserBasedRecommender
+from gruppenprojekt.collaborative_filtering_recommender import CollaborativeFilteringRecommender
 
 if __name__ == '__main__':
+
     bewertungsmatrix_df = pd.read_csv('data/Bewertungsmatrix_FlixNet.csv')
-    r = UserBasedRecommender(data=bewertungsmatrix_df)
-    r.predict(user_id="741", item_id="104", calculation_variety="ungewichtet", k=3)
-    r.predict(user_id="5378", item_id="1073", k=5)
+
+    item_based_recommender = CollaborativeFilteringRecommender(data=bewertungsmatrix_df, mode='item')
+    item_based_recommender.predict(user_id="741", item_id="1088", k=10)
+    item_based_recommender.predict(user_id="5378", item_id="1088", k=2)
+
+    user_based_recommender = CollaborativeFilteringRecommender(data=bewertungsmatrix_df, mode='user')
+    user_based_recommender.predict(user_id="741", item_id="104", calculation_variety="unweighted", k=3)
+    user_based_recommender.predict(user_id="5378", item_id="1073", k=2)
